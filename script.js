a9b2d2ceee3a873231a78d4082fea787fc919037
Matheus Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2022-07-05 10:34:39 -0300

    testando gráfico

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-
-let data = [{ "day": "mon", "amount": 17.45 },
-{ "day": "tue", "amount": 34.91 },
-{ "day": "wed", "amount": 52.36 },
-{ "day": "thu", "amount": 31.07 },
-{ "day": "fri", "amount": 23.39 },
-{ "day": "sat", "amount": 43.28 },
-{ "day": "sun", "amount": 25.48 }];
-
-let myData = JSON.stringify(data)
-console.log(myData)
-
+var myData = [
+    {
+        x: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+        y: [17.45, 34.91, 52.36, 31.07, 23.39, 43.39, 43.28, 25.48],
+        type = 'bar'
+    }
+];
+Plotly.newPloty('tester', myData)
